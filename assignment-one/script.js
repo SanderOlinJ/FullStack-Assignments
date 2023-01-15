c9127aa43761c3536d29b07ddbe2157b25a911d0
Sander Johansen <sander.olin@gmail.com>
2023-01-15 21:52:36 +0100

    Added script for randomizing array of words to screen.

--- assignment-one/script.js
+++ assignment-one/script.js
@@ -1,6 +1,15 @@
 let count = 0;
 let content = document.getElementById("lorem-text");
-
+let array = [document.getElementById("li-1"),
+document.getElementById("li-2"),
+document.getElementById("li-3"),
+document.getElementById("li-4"),
+document.getElementById("li-5"),
+document.getElementById("li-6")];
+const words = ["lorem", "ipsum", "dolor", "sit", "amet",
+"consectetur", "adipiscing", "elit", "sed", "do", "eiusmod",
+"tempor", "incididunt", "ut", "labore", "et", "dolore",
+"magna", "aliqua"];
 
 function updateCounter(){
     let counter = document.getElementById("counter");
@@ -22,5 +31,22 @@ function toggleContent(){
         content.style.opacity = '0';
     } else {
         content.style.opacity = '1';
+    }
+}
+
+function chooseFourRandomWordsFromArray(){
+    let fourWord = [];
+    for (let i = 0; i < 6; i++){
+        let randomElement = words[Math.floor(Math.random() * array.length)];
+        fourWord[i] = randomElement;
+    }
+    return fourWord;
+}
+
+function fillArrayWithWords(){
+    const wordsGenerated = chooseFourRandomWordsFromArray();
+    for (let i = 0; i < array.length; i++){
+        let word = wordsGenerated[i];
+        array[i].innerText = word;
     }
 }
